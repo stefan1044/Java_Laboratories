db2095024f2301fcb01582d88b63d39d68374172
hara <stefan.harabagiu2000@gmail.com>
2023-04-10 15:05:24 +0300

    lab7 compulsory fixes

--- lab7/Robot.java
+++ lab7/Robot.java
@@ -2,21 +2,21 @@ package lab7;
 
 import static java.lang.Thread.sleep;
 
-public class Robot implements Runnable{
+public class Robot implements Runnable {
     private final String name;
-    private boolean running;
+    private final int x;
+    private final int y;
     Exploration exploration;
-    private int x, y;
+    private boolean running;
 
-    Robot(String name, Exploration exploration, int x, int y){
+    Robot(String name, Exploration exploration, int x, int y) {
         this.name = name;
-        this.exploration =exploration;
+        this.exploration = exploration;
         this.running = true;
         this.x = x;
         this.y = y;
-        this.exploration.getExplorationMap().visit(x,y,this);
+        this.exploration.getExplorationMap().visit(x, y, this);
     }
-
 
 
     public String getName() {
@@ -27,28 +27,26 @@ public class Robot implements Runnable{
     public void run() {
         int[] rowDirections = {-1, 0, 1, 0};
         int[] colDirections = {0, 1, 0, -1};
-        while (this.running){
+        while (this.running) {
             boolean couldMove = false;
-            for(int i = 0;i<4;i++){
-                if ( 0<=this.x + rowDirections[i] && this.x + rowDirections[i] <
-                        this.exploration.getExplorationMap().getSharedMemory().getN() && 0<=this.y + colDirections[i] &&
-                        this.y + colDirections[i] < this.exploration.getExplorationMap().getSharedMemory().getN()) {
-                    if (this.exploration.getExplorationMap().visit(this.x + rowDirections[i],
-                            this.y + colDirections[i], this)) {
-
+            for (int i = 0; i < 4; i++) {
+                var upperBound = this.exploration.getExplorationMap().getSharedMemory().getN();
+                var newX = this.x + rowDirections[i];
+                var newY = this.y + colDirections[i];
+                if (0 <= newX && newX < upperBound && 0 <= newY && newY< upperBound) {
+                    if (this.exploration.getExplorationMap().visit(newX, newY, this)) {
                         couldMove = true;
                         break;
                     }
                 }
             }
 
-            if (!couldMove)
-                this.running = false;
+            if (!couldMove) this.running = false;
 
             try {
-                sleep((int) (Math.random()*100));
+                sleep((int) (Math.random() * 100));
             } catch (InterruptedException e) {
-                System.out.println(e);;
+                System.out.println(e);
             }
         }
     }
